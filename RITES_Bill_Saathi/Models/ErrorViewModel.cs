95917fae9e3934bfdd0358a3683040ce22cc91c9
Joginder Tanikella <joginder.tanikella@gmail.com>
2024-03-19 11:49:41 +0530

    Working

---
+++ RITES_Bill_Saathi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RITES_Bill_Saathi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
